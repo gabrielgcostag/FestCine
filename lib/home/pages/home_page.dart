1108dcd5bc347a7f92557b7e8a42a10d7193f660
VBruninhu <bruno.vasel2010@gmail.com>
2024-03-13 10:00:23 -0300

    Drawer Created

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -21,9 +21,21 @@ class _HomePageState extends State<HomePage> {
     pc = PageController(initialPage: paginaAtual);
   }
 
-  setPaginaAtual(pagina) {
+  void _onBottomNavItemTapped(int index) {
     setState(() {
-      paginaAtual = pagina;
+      paginaAtual = index;
+      pc.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 400),
+        curve: Curves.ease,
+      );
+    });
+  }
+
+  void _onDrawerItemTapped(int index) {
+    setState(() {
+      paginaAtual = index;
+      pc.jumpToPage(index);
     });
   }
 
@@ -49,12 +61,83 @@ class _HomePageState extends State<HomePage> {
               const Divider(height: 0, indent: 16, endIndent: 16),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: ListView(
+                child: Column(
                   children: [
                     ListTile(
+                      leading: const Icon(Icons.list),
                       title: const Text('Todas'),
-                      onTap: () {},
-                    )
+                      onTap: () {
+                        _onDrawerItemTapped(0);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.photo),
+                      title: const Text('Galeria'),
+                      onTap: () {
+                        _onDrawerItemTapped(1);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.person),
+                      title: const Text('Indicados'),
+                      onTap: () {
+                        _onDrawerItemTapped(2);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text('Configurações'),
+                      onTap: () {
+                        _onDrawerItemTapped(3);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    const Divider(indent: 8, endIndent: 8),
+                    ListTile(
+                      leading: const Icon(Icons.poll),
+                      title: const Text('Outra Page'),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.poll),
+                      title: const Text('Outra Page'),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.poll),
+                      title: const Text('Outra Page'),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ],
                 ),
               )
@@ -64,11 +147,13 @@ class _HomePageState extends State<HomePage> {
       ),
       body: PageView(
         controller: pc,
-        onPageChanged: setPaginaAtual,
+        onPageChanged: (index) => setState(() {
+          paginaAtual = index;
+        }),
         children: const [
           SchedulePage(),
-          IndicatedPage(),
           GalleryPage(),
+          IndicatedPage(),
           Scaffold(),
         ],
       ),
@@ -80,19 +165,13 @@ class _HomePageState extends State<HomePage> {
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todas'),
           BottomNavigationBarItem(icon: Icon(Icons.photo), label: 'Galeria'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Favoritas'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Indicados'),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Configuração',
           ),
         ],
-        onTap: (pagina) {
-          pc.animateToPage(
-            pagina,
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.ease,
-          );
-        },
+        onTap: _onBottomNavItemTapped,
       ),
     );
   }
