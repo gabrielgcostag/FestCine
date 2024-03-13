747b6972962c857e0a3355675bdfe6e447c47202
Gabriel Gonçalves <gabrielgcostag@gmail.com>
2024-03-13 11:48:17 -0300

    SchedulePage Fix

--- lib/programacao/pages/schedule_page.dart
+++ lib/programacao/pages/schedule_page.dart
@@ -15,93 +15,60 @@ class SchedulePage extends StatefulWidget {
 class _SchedulePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       extendBodyBehindAppBar: true,
       backgroundColor: primaryColor,
-      appBar: AppBar(
-        title: const Text(
-          'PROGRAMAÇÃO',
-          style: TextStyle(fontFamily: 'Montserrat', fontSize: 18),
-        ),
-        backgroundColor: Colors.transparent,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return Padding(
-              padding: EdgeInsets.symmetric(vertical: 24),
-              child: OutlinedButton(
-                onPressed: () {
-                  context.read<AuthService>().logout();
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => WelcomePage()));
-                },
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.red,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Text(
-                        'Sair do App',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 120,
+              ),
+              Center(
+                child: Text(
+                  'Confira a programação',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: tertiaryColor,
+                      fontSize: 25),
                 ),
               ),
-            );
-          },
-        ),
-      ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 120,
-            ),
-            Center(
-              child: Text(
-                'Confira a programação',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    color: tertiaryColor,
-                    fontSize: 25),
+              SizedBox(
+                height: 40,
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao1.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao2.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao3.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao4.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao5.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao6.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao7.webp')),
-            SizedBox(
-              height: 40,
-            ),
-            Image(image: AssetImage('assets/images/programacao8.webp'))
-          ],
+              Image(image: AssetImage('assets/images/programacao1.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao2.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao3.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao4.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao5.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao6.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao7.webp')),
+              SizedBox(
+                height: 40,
+              ),
+              Image(image: AssetImage('assets/images/programacao8.webp'))
+            ],
+          ),
         ),
       ),
     );
