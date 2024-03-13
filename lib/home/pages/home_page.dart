23f8b0748ac819f9da7388b27e2d453ec3cd066c
VBruninhu <bruno.vasel2010@gmail.com>
2024-03-13 01:52:57 -0300

    Drawer created

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -2,14 +2,13 @@ import 'package:festcine_pedraazul/core/helpers/colors.dart';
 import 'package:festcine_pedraazul/galeria/pages/gallery_page.dart';
 import 'package:festcine_pedraazul/indicados/pages/indicated_page.dart';
 import 'package:festcine_pedraazul/programacao/pages/schedule_page.dart';
-import 'package:festcine_pedraazul/salas_de_exibicao/pages/exhibition_room.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -31,6 +30,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        iconTheme: const IconThemeData(color: tertiaryColor),
+      ),
+      extendBodyBehindAppBar: true,
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  'Navegação',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const Divider(height: 0, indent: 16, endIndent: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: const Text('Todas'),
+                      onTap: () {},
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       body: PageView(
         controller: pc,
         onPageChanged: setPaginaAtual,
