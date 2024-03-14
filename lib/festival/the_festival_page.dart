e2f43c963dc8d8633d7527669e20bdf5f7448c68
Gabriel Gonçalves <gabrielgcostag@gmail.com>
2024-03-14 20:54:13 -0300

    add some features

--- lib/festival/the_festival_page.dart
+++ lib/festival/the_festival_page.dart
@@ -15,44 +15,56 @@ class TheFestivalPage extends StatefulWidget {
 }
 
 class _TheFestivalPageState extends State<TheFestivalPage> {
-  bool selected = false;
+  bool selected = true;
   @override
   Widget build(BuildContext context) {
     final currentWidth = MediaQuery.of(context).size.width;
     final currentHeight = MediaQuery.of(context).size.height;
-    return SizedBox(
-        child: Stack(children: <Widget>[
-      Container(
-        width: currentWidth,
-        height: currentHeight,
-        color: primaryColor,
-        child: Image(image: AssetImage("assets/images/sobre_canva.png")),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        forceMaterialTransparency: true,
       ),
-      AnimatedPositioned(
-        width: selected ? currentWidth : currentWidth,
-        height: selected ? currentHeight : 100,
-        top: selected ? 0 : 0,
-        duration: const Duration(seconds: 2),
-        curve: Curves.fastOutSlowIn,
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              selected = !selected;
-            });
-          },
-          child: Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-            child: const ClipRRect(
-              borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(20), top: Radius.zero),
-              child: Image(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/images/encontro_arte_natureza.png"),
+      body: SizedBox(
+          child: Stack(children: <Widget>[
+        Container(
+          width: currentWidth,
+          height: currentHeight,
+          color: primaryColor,
+          child: Image(image: AssetImage("assets/images/sobre_canva.png")),
+        ),
+        AnimatedPositioned(
+          width: selected ? currentWidth : currentWidth,
+          height: selected ? currentHeight : 100,
+          top: selected ? 0 : 0,
+          duration: const Duration(seconds: 2),
+          curve: Curves.fastOutSlowIn,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selected = !selected;
+              });
+            },
+            onVerticalDragStart: (details) {
+              setState(() {
+                selected = !selected;
+              });
+            },
+            child: Container(
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(10)),
+              child: const ClipRRect(
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(20), top: Radius.zero),
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/encontro_arte_natureza.png"),
+                ),
               ),
             ),
           ),
         ),
-      ),
-    ]));
+      ])),
+    );
   }
 }
